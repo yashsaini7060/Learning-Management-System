6e0fb96a41feb4f7ae7159bd1c167d542c958d3f
yashsaini7060 <yk18052000@gmail.com>
2025-01-16 03:31:20 +0530

    changed axios base url

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create();
 console.log(process.env.BASE_URL)
-axiosInstance.defaults.baseURL = process.env.BASE_URL || 'https://learning-management-system-phi-black.vercel.app//api/v1' || "http://localhost:5014/api/v1";
+axiosInstance.defaults.baseURL = process.env.BASE_URL || 'https://learning-management-system-production-1948.up.railway.app/api/v1' || "http://localhost:5014/api/v1";
 axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
