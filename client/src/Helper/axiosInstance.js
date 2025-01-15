007807bf5dcbde1500245d5a9cb6d1ed3b58e943
yashsaini7060 <yk18052000@gmail.com>
2025-01-16 00:38:49 +0530

    axios base url updated

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create();
-
-axiosInstance.defaults.baseURL = process.env.BASE_URL || "http://localhost:5014/api/v1";
+console.log(process.env.BASE_URL)
+axiosInstance.defaults.baseURL = process.env.BASE_URL || 'https://learning-management-system-production-1948.up.railway.app/api/v1' || "http://localhost:5014/api/v1";
 axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
